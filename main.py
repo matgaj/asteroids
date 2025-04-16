7159be1d10c4890e4e2c7ee99b3e7f80a7fe26bb
--global <--global>
2025-04-16 17:07:57 +0200

    remove unused print statements

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from asteroidfield import AsteroidField
 from shot import Shot
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     # Initialize the game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
